12d9b1ec977d569d84010f2961465202a13416cc
Nikroo <curiosityopportunity@gmail.com>
2021-09-21 17:24:14 +0300

    Initial maven project

---
+++ src/main/java/by/itacademy/jee/maven/App.java
@@ -0,0 +1,12 @@
+package by.itacademy.jee.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+
+        System.out.println( "Hello World!" );
+    }
+}
